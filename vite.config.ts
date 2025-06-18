3e91b3e63ca59fdd8219ddd1b3b10ec239a831d8
Hernán Espinoza <hernan.espinoza.dev@gmail.com>
2025-06-17 22:10:41 -0400

    modificaciones para githubpages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Añadimos la base para el despliegue en GitHub Pages
+  base: '/automotriz-prototipo/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
